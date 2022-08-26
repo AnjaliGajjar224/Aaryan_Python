e60f4e07eb895fc43d19afd996c1423f93995a3c
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-26 07:57:21 +0530

    Add files via upload

--- while.py
+++ while.py
@@ -58,20 +58,20 @@ n == sum ---> Armstrong Numbers
 
 
 
-n = int(input("Enter a number you want to check: "))
+# n = int(input("Enter a number you want to check: "))
 
-sum = 0
-temp = n
+# sum = 0
+# temp = n
 
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** len(str(n))    # sum = sum + digit ** 3 
-    temp //= 10   # temp = temp // 10
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit ** len(str(n))    # sum = sum + digit ** 3 
+#     temp //= 10   # temp = temp // 10
 
-if sum == n:
-    print("Armstrong")
-else:
-    print("Not Armstrong")
+# if sum == n:
+#     print("Armstrong")
+# else:
+#     print("Not Armstrong")
 
 """
 1. Check whether the number is Palindrome or not.
@@ -88,4 +88,186 @@ e.g. n = 123
     sum == mul ---> Twin number
 
 3. Check whether the number is Prime or not.
+
 """
+# n = int(input("Enter a number you want to check: "))
+
+# rev = 0
+# temp = n 
+
+# while temp > 0:
+#   digit = temp % 10
+#   rev = rev * 10 + digit
+#   temp //= 10   # temp = temp // 10
+
+# if n == rev:
+#   print("Palindrome")
+# else:
+#   print("Not Palindrome")
+
+"""
+Nested while loop:
+------------------
+
+initialization
+
+while condition:
+  initialization
+  while condition:
+    statements
+    incremet/decrement
+  increment/decrement
+"""
+
+# i = 1
+
+# while i <= 5:
+#   j = 1
+#   while j <= 5:
+#     print("*",end=" ")
+#     j += 1
+#   print()
+#   i += 1
+
+"""
+1 1 1 1 1 
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
+"""
+
+# i = 1
+# num = int(input("Number: "))
+# n = 1
+
+# while i <= num:
+#   j = 1
+#   while j <= num:
+#     print(n,end=" ")
+#     n += 1
+#     j += 1
+#   print()
+#   i += 1
+
+"""
+A A A A A
+B B B B B
+C C C C C
+D D D D D
+E E E E E
+"""
+
+# ch = 65
+
+# print(chr(ch))
+
+# i = 0
+# ch = 65
+# while i < 5:
+#   j = 1
+#   while j <= 5:
+#     print(chr(ch + i),end=" ")
+#     j += 1
+#   print()
+#   i += 1
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+1 2 3 4 5 
+2 3 4 5
+3 4 5
+4 5 
+5
+
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1 
+1
+
+     *
+   * * *
+ * * * * *
+* * * * * * 
+"""
+"""
+Control statements
+1. break
+2. continue
+3. pass
+"""
+
+# i = 1
+
+# while i <= 10:
+#     if i == 5:
+#       break
+#     print(i)
+#     i += 1
+
+# i = 1
+
+# while i <= 10:
+#     i += 1
+
+#     if i == 5:
+#       continue
+#     print(i)
+
+# i =1
+
+# while i <= 5:
+#   pass
+#   i += 1
+
+"""
+while...else
+"""
+
+# i = 1
+
+# while i <= 10:
+#   print(i)
+#   i += 1
+# else:
+#   print("Loop is over")
+
+i = 1
+
+while i <= 10:
+  print(i)
+  if i == 5:
+    break
+  i += 1
+else:
+  print("Loop is over")
